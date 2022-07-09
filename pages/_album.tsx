47d604ac020fd7cb82f5b0729efae3eb34520580
Alice Huang <alicehccn@gmail.com>
2022-07-09 15:10:23 -0700

    testing ci pipline

--- pages/_album.tsx
+++ pages/_album.tsx
@@ -3,7 +3,6 @@ import styles from '../styles/Home.module.css'
 import Note from './_note';
 
 const Album: NextPage = () => {
-  const date = new Date().toDateString();
   return (
     <div className={styles.container}>
       <Note />
